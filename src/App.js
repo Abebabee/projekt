32a1b66c309a00e6eb73837233e8b01850d77900
Niklas Sjöstedt <niklassjostedt341@gmail.com>
2021-11-17 17:02:22 +0100

    react hämtning

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import logo from './logo.svg';
+
+import Axios from 'axios'
+import React,{useState,useEffect} from 'react';
 
 function App() {
+  const [data,setData]=useState([])
+  useEffect(() => {
+    fetchData();
+  }, [])
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+  const fetchData=async()=>{
+    const response=await Axios('https://fakestoreapi.com/products');
+    setData(response.data)    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with abe, coff and nike
-          we learn lots of stuff yaesss
-          Hi my name is nike this is me :)
-        </a>
-      </header>
+      {
+        data && data.map(data=>{
+          return(
+            <div key={data.id} style={{alignItems:'center',margin:'20px 60px'}}>
+            <img src = {data.image} alt = 'Hej'></img>
+            <p>{data.email}</p>
+          </div>
+          )
+        })
+      }
     </div>
   );
 }
-window.addEventListener('load',function(){
-  fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(data=>{
-              console.log(data);
-              let count =0;
-              Object.keys(data).forEach(elements=>{
-                count++;
-                //console.log(count);
-                return <img src={data.count.image} alt="en bild"></img>
-              });
-            });
-});
 
 export default App;
